13dfe3cc282641998d3eae19d5e77b6d1ab935c1
Astrocat2374 <sugiyama.evan@gmail.com>
2018-09-07 13:22:36 -1000

    Done

--- app.js
+++
@@ -1,8 +0,0 @@
-var num = [1,2,3,4];
-var sum = 0;
-
-num.forEach(function(i) {
-    sum += i;
-})
-
-console.log(sum);
---
+++ let-const.js
@@ -0,0 +1,18 @@
+//const
+const name = 'Evan';
+console.log(name);
+
+//--------------------------------------------------------------------------------------------
+
+//let
+let dog = 'hotdog';
+dog = 'sausage';
+console.log(dog);
+
+var greetingSentence = "Hewwo OwO";
+
+var greeting = () => {
+    console.log(greetingSentence);
+}
+
+greeting();
